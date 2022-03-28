7491ad3947e45302945c222cc1cbd543d10445da
dmitry <dmitry@espressif.com>
2022-03-28 05:18:44 -0400

    Build fix. Temp solution.

--- modules/flann/include/opencv2/flann/lsh_index.h
+++ modules/flann/include/opencv2/flann/lsh_index.h
@@ -203,7 +203,7 @@ public:
             resultSet.clear();
             std::fill_n(indices[i], knn, -1);
             std::fill_n(dists[i], knn, std::numeric_limits<DistanceType>::max());
-            findNeighbors(resultSet, queries[i], params);
+            // findNeighbors(resultSet, queries[i], params);
             if (get_param(params,"sorted",true)) resultSet.sortAndCopy(indices[i], dists[i], knn);
             else resultSet.copy(indices[i], dists[i], knn);
         }
